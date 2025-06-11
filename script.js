5443bbc2d01d24abab2117adb97cb00909056a75
DaizuBuilding <162403102+DaizuBuilding@users.noreply.github.com>
2025-06-12 02:12:09 +0900

    update

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 var dateObject = new Date();
 var year = dateObject.getFullYear();
-var month = dateObject.getMonth() + 1;
-var date = dateObject.getDate() + 1;
-var hours = dateObject.getHours();
-var minutes = dateObject.getMinutes();
+var month = lengthFix(dateObject.getMonth() + 1);
+var date = lengthFix(dateObject.getDate() + 1);
+var hours = lengthFix(dateObject.getHours());
+var minutes = lengthFix(dateObject.getMinutes());
 var dayObject = dateObject.getDay() + 1;
 const dayArray = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-var day = dayArray(dayObject);
+var day = dayArray[dayObject];
 
-var timeHTML = document.getElementById('time');
+var timeHTML = document.getElementById("time");
 timeHTML.innerHTML = String(hours) + ':' + String(minutes);
+
+var number = 0;
+
+function lengthFix(number) {
+    if (number< 10) {
+        return '0' + String(number);
+    } else {
+        return String(number);
+    }
+}
